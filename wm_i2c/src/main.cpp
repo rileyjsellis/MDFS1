49ce5e095245945ad4e96e943497bc2c82acac3c
rileyjsellis <143316774+rileyjsellis@users.noreply.github.com>
2024-05-16 21:00:02 +1000

    formatting to be in line with Google's indentation conventions

--- wm_i2c/src/main.cpp
+++ wm_i2c/src/main.cpp
@@ -29,45 +29,47 @@ void setup() {
 
 
 void readInputs(){
-    i_joy_x = byte(analogRead(map(kJoyXPin,1,1024,1,255))); //reducing to send.
-    i_joy_y = byte(analogRead(map(kJoyYPin,1,1024,1,255)));
-    Serial.print(digitalRead(kJoyBtnPin));
+  i_joy_x = byte(analogRead(map(kJoyXPin,1,1024,1,255))); //reducing to send.
+  i_joy_y = byte(analogRead(map(kJoyYPin,1,1024,1,255)));
+  Serial.print(digitalRead(kJoyBtnPin));
 }
 
 void stateChangeDebounce(){
-    if (digitalRead(kJoyBtnPin) == LOW && millis() - us_button_timer > 1000) {
-        by_state++;
-        us_button_timer = millis();
-        if (by_state == 3) {
-            by_state = 0;
-        }
+  const int i_delay = 1000;
+
+  if (digitalRead(kJoyBtnPin) == LOW && millis() - us_button_timer > i_delay) {
+    by_state++;
+    us_button_timer = millis();
+    if (by_state == kWireArrayLength) {
+      by_state = 0;
     }
+  }
 }
 
 void sendData(){
-    byte data[kWireArrayLength];
-    data[0] = i_joy_x;
-    data[1] = i_joy_y;
-    data[2] = by_state;     
-    Serial.println(data[2]); //diagnostic println
-    Wire.beginTransmission(kWireAddress);
-    Wire.write(data, kWireArrayLength);
-    Wire.endTransmission();
+  byte data[kWireArrayLength];
+  data[0] = i_joy_x;
+  data[1] = i_joy_y;
+  data[2] = by_state;     
+  Serial.println(data[2]); //diagnostic println
+  Wire.beginTransmission(kWireAddress);
+  Wire.write(data, kWireArrayLength);
+  Wire.endTransmission();
 }
 
 void sendInputsData(){
-    unsigned long us_current_time = millis();
-    unsigned long us_time_since_last_send;
-    int sending_interval = 300;
+  unsigned long us_current_time = millis();
+  unsigned long us_time_since_last_send;
+  int sending_interval = 300;
 
-    if (us_current_time - us_time_since_last_send > sending_interval){
-        readInputs();
-        stateChangeDebounce();
-        sendData();
-        us_time_since_last_send = us_current_time;
-    }
+  if (us_current_time - us_time_since_last_send > sending_interval){
+    readInputs();
+    stateChangeDebounce();
+    sendData();
+    us_time_since_last_send = us_current_time;
+  }
 }
 
 void loop() {
-    sendInputsData();
+  sendInputsData();
 }
--- wm_steppers/src/main.cpp
+++ wm_steppers/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-
 //1. pin assignments
 const byte kEnPin = 8;
 const byte kStepXPin = 2;
@@ -27,10 +26,10 @@ const int kJoyNeutral = 255/2; //half of byte recieved
 
 //*** Riley's note, to review enum possibility
 enum dc_motor{ 
-    front_left=1,
-    front_right=2,
-    back_left=3,
-    back_right=4
+  front_left=1,
+  front_right=2,
+  back_left=3,
+  back_right=4
 };
 
 // stepper
@@ -92,57 +91,57 @@ void stepperSelection(){
 }
 
 void stepperDirection(){
-    const int kJoyBuffer = kJoyNeutral/20; //2.5% buffer,
-    const int kJoyBufferLower = kJoyNeutral - kJoyBuffer;
-    const int kJoyBufferUpper = kJoyNeutral + kJoyBuffer;
+  const int kJoyBuffer = kJoyNeutral/20; //2.5% buffer,
+  const int kJoyBufferLower = kJoyNeutral - kJoyBuffer;
+  const int kJoyBufferUpper = kJoyNeutral + kJoyBuffer;
 
-    if ((i_stepper_direction > kJoyBufferLower) && (i_stepper_direction < kJoyBufferUpper)){
-        digitalWrite(i_step_pin, LOW);
-        return;
-    }
+  if ((i_stepper_direction > kJoyBufferLower) && (i_stepper_direction < kJoyBufferUpper)){
+    digitalWrite(i_step_pin, LOW);
+    return;
+  }
 
-    bool b_direction_forward = map(i_stepper_direction,1,255,0,1);
-    digitalWrite(i_step_pin, HIGH);
-    digitalWrite(i_dir_pin, b_direction_forward);
+  bool b_direction_forward = map(i_stepper_direction,1,255,0,1);
+  digitalWrite(i_step_pin, HIGH);
+  digitalWrite(i_dir_pin, b_direction_forward);
 }
 
 void stepperNextStep(){
-    if (us_current_time - us_last_stepper_motion >= f_T){
-        digitalWrite(i_step_pin, b_last_step);
-        us_last_stepper_motion = us_current_time;
-        b_last_step = !b_last_step;
-    }
+  if (us_current_time - us_last_stepper_motion >= f_T){
+    digitalWrite(i_step_pin, b_last_step);
+    us_last_stepper_motion = us_current_time;
+    b_last_step = !b_last_step;
+  }
 }
 
 void stepperMoving(){ //current function work
-    for (int i = 0; i < (i_steps * 2); i++) {
-        stepperNextStep();
-    }
+  for (int i = 0; i < (i_steps * 2); i++) {
+    stepperNextStep();
+  }
 }
 
 float setRPM(int rpm, float res){ //assuming 150RPM, 0.9deg res
-    float freq = (float)rpm/((res/360)*60); //freq = 1000
-    float period = 1/freq; //period = 1/1000
-    return (period*0.5)*1000; //T = 500 microseconds
+  float freq = (float)rpm/((res/360)*60); //freq = 1000
+  float period = 1/freq; //period = 1/1000
+  return (period*0.5)*1000; //T = 500 microseconds
 }
 
 //RPM
 void readRPM(){
-    if(Serial.available() > 0){
-        s_RPM = Serial.readString();
-        f_T = setRPM(s_RPM.toInt(), f_resolution);
-        Serial.print(s_RPM);
-    }
+  if(Serial.available() > 0){
+    s_RPM = Serial.readString();
+    f_T = setRPM(s_RPM.toInt(), f_resolution);
+    Serial.print(s_RPM);
+  }
 }
 
 void stepperProcess(){
-    //readRPM(); //not quite needed for now
-    stepperSelection(); 
-    stepperDirection(); 
-    stepperMoving();
+  //readRPM(); //not quite needed for now
+  stepperSelection(); 
+  stepperDirection(); 
+  stepperMoving();
 }
 
 void loop() {
-    us_current_time = millis();
-    stepperProcess();
+  us_current_time = millis();
+  stepperProcess();
 }
